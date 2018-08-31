e08d9f34987c554126ebb41e50b234dd405ac4b9
Unknown <imototoriwatomodatijanai@gmail.com>
2018-08-31 20:21:04 +0900

    first commit

---
+++ mask.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# vim: set foldmethod=marker commentstring=\ \ #\ %s :
+#
+# Author:    Taishi Matsumura
+# Created:   2018-08-19
+#
+# Copyright (C) 2018 Taishi Matsumura
+#
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+import PIL.Image
+import numpy as np
+import time
+import io
+import base64
+
+header = html.Header(html.H1('Mask Create Tool'))
+
+uploader = dcc.Upload(
+        id='uploader',
+        children=html.Div(['Drag and Drop or ', html.A('Select A File')]),
+        style={
+            'width': '800px',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'},
+        multiple=False)
+
+n_rows = dcc.Input(
+        id='n-rows', type='number', value=8, max=100, min=0, size=5)
+n_clms = dcc.Input(
+        id='n-clms', type='number', value=12, max=100, min=0, size=5)
+n_plates = dcc.Input(
+        id='n-plates', type='number', value=3, max=10, min=0, size=5)
+row_gap = dcc.Input(
+        id='row-gap', type='number', value=1, max=10, min=0, size=5)
+clm_gap = dcc.Input(
+        id='clm-gap', type='number', value=1, max=10, min=0, size=5)
+plate_gap = dcc.Input(
+        id='plate-gap', type='number', value=71, max=100, min=0, size=5)
+x = dcc.Input(
+        id='x', type='number', value=0, max=1500, min=0, size=5)
+y = dcc.Input(
+        id='y', type='number', value=0, max=1500, min=0, size=5)
+well_w = dcc.Input(
+        id='well_w', type='number', value=0, max=1500, min=0, size=5)
+well_h = dcc.Input(
+        id='well_h', type='number', value=0, max=1500, min=0, size=5)
+
+input_div = html.Div(
+        id='input-div',
+        children=[
+            uploader, 'test string', n_rows, n_clms, n_plates,
+            row_gap, clm_gap, plate_gap, x, y, well_w, well_h])
+
+org_div = html.Div(
+        [dcc.Graph(id='org-img')],
+        id='org-div',
+        style={'display': 'inline-block'})
+
+mask_div = html.Div(id='mask-div', style={'display': 'inline-block'})
+
+images_div = html.Div(id='images-div', children=[org_div, mask_div])
+
+app = dash.Dash()
+app.layout = html.Div([header, input_div, images_div])
+app.css.append_css(
+        {'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+@app.callback(
+        Output('org-div', 'children'),
+        [Input('uploader', 'contents')])
+def update_images_div(data_uri):
+    if data_uri is None:
+        return
+    imghash = data_uri.split(',')[1]
+    img = PIL.Image.open(io.BytesIO(base64.b64decode(imghash)))
+    height = np.array(img).shape[0]
+    width = np.array(img).shape[1]
+    graph = dcc.Graph(
+        id='org-img',
+        figure = {
+            'data': [go.Scatter(x=[0], y=[0], mode='lines+markers')],
+            'layout': {
+                'width': 500,
+                'height': 800,
+                'margin': go.layout.Margin(l=40, b=40, t=26, r=10),
+                'xaxis': {
+                    'range': (0, width),
+                    'scaleanchor': 'y',
+                    'scaleratio': 1,
+                },
+                'yaxis': {
+                    'range': (0, height),
+                },
+                'images': [{
+                    'xref': 'x',
+                    'yref': 'y',
+                    'x': 0,
+                    'y': 0,
+                    'yanchor': 'bottom',
+                    'sizing': 'stretch',
+                    'sizex': width,
+                    'sizey': height,
+                    'source': data_uri,
+                }],
+                'dragmode': 'select',
+            }
+        },
+        style={'display': 'inline-block'})
+    return [graph]
+
+@app.callback(
+        Output('x', 'value'),
+        [Input('org-img', 'selectedData')])
+def update_x(selected_data):
+    if selected_data is None:
+        return
+    range_x = np.array(selected_data['range']['x']).astype(int)
+    return range_x[0]
+
+@app.callback(
+        Output('y', 'value'),
+        [Input('org-img', 'selectedData')])
+def update_y(selected_data):
+    if selected_data is None:
+        return
+    range_y = np.array(selected_data['range']['y']).astype(int)
+    return range_y[0]
+
+@app.callback(
+        Output('well_w', 'value'),
+        [Input('org-img', 'selectedData')])
+def update_well_w(selected_data):
+    if selected_data is None:
+        return
+    range_x = np.array(selected_data['range']['x']).astype(int)
+    return range_x[1] - range_x[0]
+
+@app.callback(
+        Output('well_h', 'value'),
+        [Input('org-img', 'selectedData')])
+def update_well_h(selected_data):
+    if selected_data is None:
+        return
+    range_y = np.array(selected_data['range']['y']).astype(int)
+    return range_y[1] - range_y[0]
+
+@app.callback(
+        Output('mask-div', 'children'),
+        [Input('n-rows', 'value'),
+            Input('n-clms', 'value'),
+            Input('n-plates', 'value'),
+            Input('row-gap', 'value'),
+            Input('clm-gap', 'value'),
+            Input('plate-gap', 'value'),
+            Input('x', 'value'),
+            Input('y', 'value'),
+            Input('well_w', 'value'),
+            Input('well_h', 'value')],
+        [State('org-img', 'figure')])
+def draw_images(
+        n_rows, n_clms, n_plates,
+        gap_r, gap_c, gap_p, x, y, well_w, well_h, figure):
+    if figure is None:
+        return
+    # Get base64ed hash of original image
+    orgimg_uri = figure['layout']['images'][0]['source']
+    imghash = orgimg_uri.split(',')[1]
+
+    # Transform hash to ndarray
+    org_img = np.array(PIL.Image.open(io.BytesIO(base64.b64decode(imghash))))
+
+    # Parameters
+    height, width = org_img.shape[0], org_img.shape[1]
+    count = 0
+
+    # Mask create loop
+    mask = -1 * np.ones_like(org_img)
+    for n in range(n_plates):
+        for idx_r in range(n_rows):
+            for idx_c in range(n_clms):
+                c1 = x + idx_c*(well_w + gap_c)
+                c2 = c1 + well_w
+                r1 = y + idx_r*(well_h + gap_r) + n*(n_rows*well_h + gap_p) + gap_r*(n - 1)
+                r2 = r1 + well_h
+                mask[r1:r2, c1:c2] = count
+                count += 1
+
+    grayed = PIL.Image.fromarray(
+            np.flipud(np.where(mask>=0, 255, 0).astype(np.uint8)))
+    masked = PIL.Image.fromarray(
+            np.flipud(np.where(mask>=0, 1, 0).astype(np.uint8)) * org_img)
+    buff1 = io.BytesIO()
+    buff2 = io.BytesIO()
+    grayed.save(buff1, format='PNG')
+    masked.save(buff2, format='PNG')
+
+    np.save('static/mask.npy', mask.astype(np.int16))
+
+    graph2 = dcc.Graph(
+        id='graph2',
+        figure={
+            'data': [go.Scatter(x=[0], y=[0], mode='lines+markers')],
+            'layout': {
+                'width': 500,
+                'height': 800,
+                'margin': go.layout.Margin(l=40, b=40, t=26, r=10),
+                'xaxis': {
+                    'range': (0, width),
+                    'scaleanchor': 'y',
+                    'scaleratio': 1,
+                },
+                'yaxis': {
+                    'range': (0, height),
+                },
+                'images': [{
+                    'xref': 'x',
+                    'yref': 'y',
+                    'x': 0,
+                    'y': 0,
+                    'yanchor': 'bottom',
+                    'sizing': 'stretch',
+                    'sizex': width,
+                    'sizey': height,
+                    'source': 'data:image/png;base64,{}'.format(
+                        base64.b64encode(buff1.getvalue()).decode('utf-8')),
+                }],
+                'dragmode': 'select',
+            }
+        },
+        style={'display': 'inline-block'},
+    )
+
+    graph3 = dcc.Graph(
+        id='graph3',
+        figure={
+            'data': [go.Scatter(x=[0], y=[0], mode='lines+markers')],
+            'layout': {
+                'width': 500,
+                'height': 800,
+                'margin': go.layout.Margin(l=40, b=40, t=26, r=10),
+                'xaxis': {
+                    'range': (0, width),
+                    'scaleanchor': 'y',
+                    'scaleratio': 1,
+                },
+                'yaxis': {
+                    'range': (0, height),
+                },
+                'images': [{
+                    'xref': 'x',
+                    'yref': 'y',
+                    'x': 0,
+                    'y': 0,
+                    'yanchor': 'bottom',
+                    'sizing': 'stretch',
+                    'sizex': width,
+                    'sizey': height,
+                    'source': 'data:image/png;base64,{}'.format(
+                        base64.b64encode(buff2.getvalue()).decode('utf-8')),
+                }],
+                'dragmode': 'select',
+            }
+        },
+        style={'display': 'inline-block'},
+    )
+
+    return [graph2, graph3]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ viewer.py
@@ -0,0 +1,425 @@
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+# vim: set foldmethod=marker commentstring=\ \ #\ %s :
+#
+# Author:    Taishi Matsumura
+# Created:   2018-08-29
+#
+# Copyright (C) 2018 Taishi Matsumura
+#
+import io
+import os
+import glob
+import dash
+import base64
+import PIL.Image
+import dash_auth
+import numpy as np
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+
+def centroid(binary_img):
+    rows, clms = np.where(binary_img)
+    return rows.sum() / rows.shape[0], clms.sum() / clms.shape[0]
+
+
+# -------------------
+#  Global variables
+# -------------------
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['matsu', 'dash1m0'],
+    ['kang', 'dash3360'],
+]
+
+data_root = 'G:/Research/Drosophila/data/'
+imaging_env = '171013.15.50.w1118.starvation.96well.3plates.740D-3'
+imaging_env = 'allevents'
+
+manual_evals_file = 'eclosion.csv'
+manual_evals_file = '171013.csv'
+manual_evals_file = 'pupariation.csv'
+
+theta = 50
+labels = None
+signals = None
+manual_evals = np.loadtxt(
+        os.path.join(data_root, imaging_env, 'original', manual_evals_file),
+        dtype=np.uint16,
+        delimiter=',').flatten()
+mask = np.load(
+        os.path.join(data_root, imaging_env, 'mask.npy'))
+
+# -------
+#  Main
+# -------
+app = dash.Dash()
+auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
+'''
+If you don't want to use CDN, set to True
+
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+'''
+app.scripts.config.serve_locally = False
+app.css.config.serve_locally = False
+app.css.append_css(
+        {'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+app.layout = html.Div([
+    html.Div(['Current npy file : '], id='current-npy'),
+    html.Div([
+        'Data root : {}'.format(data_root),
+        html.Br(),
+        'Imaging environment : {}'.format(imaging_env),
+        html.Br(),
+        'File name : {}'.format(manual_evals_file),
+        html.Br(),
+    ]),
+    html.Div([
+        html.Div([
+            html.H1('Well #', style={'display': 'inline-block'}),
+            html.H1(id='well-header', style={'display': 'inline-block'}),
+            ],
+            style={
+                'display': 'inline-block',
+                'margin-right': '100px',
+            },
+        ),
+        html.Div([
+            html.H4('Timestep : ', style={'display': 'inline-block'}),
+            html.H4(id='time-header', style={'display': 'inline-block'}),
+            ],
+            style={
+                'display': 'inline-block',
+                'margin-right': '100px',
+            },
+        ),
+    ]),
+    html.Div([
+        dcc.Input(
+            id='well-selector',
+            type='number',
+            value=0,
+            min=0,
+            size=5,
+        ),
+        dcc.Slider(
+            id='well-slider',
+            value=0,
+            min=0,
+            step=1,
+        )],
+        style={
+            'width': '400px',
+        },
+    ),
+    html.Div([
+        html.Img(
+            id='org-image',
+            style={
+                'background': '#555555',
+                'height': '200px',
+                'width': '200px',
+                'padding': '5px',
+                'display': 'inline-block',
+            },
+        ),
+        html.Img(
+            id='image',
+            style={
+                'background': '#555555',
+                'height': '200px',
+                'width': '200px',
+                'padding': '5px',
+                'display': 'inline-block',
+            },
+        ),
+        html.Div([
+            dcc.Dropdown(
+                id='dropdown1',
+                options=[
+                    {
+                        'label': 'matsu_signal_data_larva.npy',
+                        'value': 'matsu_signal_data_larva.npy',
+                    },
+                    {
+                        'label': 'matsu_signal_data_adult.npy',
+                        'value': 'matsu_signal_data_adult.npy',
+                    },
+                ],
+                value='matsu_signal_data_larva.npy',
+                placeholder='Select npy file...',
+                clearable=False,
+            ),
+            html.Button(
+                'Load',
+                id='button',
+            ),
+            dcc.Dropdown(
+                id='dropdown2',
+                options=[
+                    {'label': 'rise', 'value': 'rise'},
+                    {'label': 'fall', 'value': 'fall'},
+                ],
+                value='rise',
+                placeholder='Detect...',
+                clearable=False,
+            ),
+            ],
+            style={
+                'display': 'inline-block',
+                'width' : '300px',
+                'vertical-align': 'top',
+                'margin-right': '10px',
+            },
+        )],
+    ),
+    html.Div([
+        dcc.Graph(
+            id='signal-graph',
+            style={
+                'display': 'inline-block',
+                'height': '500px',
+                'width': '60%',
+                },
+        ),
+        html.Div([
+            dcc.Slider(
+                id='threshold-slider',
+                value=20,
+                min=0,
+                max=300,
+                step=1,
+                updatemode='mouseup',
+                vertical=True,
+            )],
+            style={
+                'display': 'inline-block',
+                'height': '300px',
+                'width': '5%',
+                'padding-bottom': '100px',
+            },
+        ),
+        dcc.Graph(
+            id='summary-graph',
+            style={
+                'display': 'inline-block',
+                'height': '500px',
+                'width': '35%',
+            },
+        ),
+    ]),
+],
+    style={
+        'width': '1200px',
+    },
+)
+
+
+# ------------
+#  Callbacks
+# ------------
+@app.callback(
+        Output('well-slider', 'max'),
+        [Input('current-npy', 'children')])
+def callback(_):
+    global labels
+    return len(labels) - 1
+
+
+@app.callback(
+        Output('well-selector', 'max'),
+        [Input('current-npy', 'children')])
+def callback(_):
+    global labels
+    return len(labels) - 1
+
+
+@app.callback(
+        Output('current-npy', 'children'),
+        [Input('button', 'n_clicks')],
+        [State('dropdown1', 'value')])
+def callback(n_clicks, larva_or_adult):
+    global labels, signals
+    labels = np.load(os.path.join(
+            data_root,
+            imaging_env,
+            larva_or_adult)) >= theta
+    signals = (np.diff(labels.astype(np.int8), axis=1)**2).sum(-1).sum(-1)
+    return 'Current npy file : {}'.format(larva_or_adult)
+
+
+@app.callback(
+        Output('well-slider', 'value'),
+        [Input('summary-graph', 'clickData'),
+         Input('current-npy', 'children')])
+def callback(click_data, _):
+    if click_data is None:
+        return 0
+    return click_data['points'][0]['pointNumber']
+
+
+@app.callback(
+        Output('summary-graph', 'figure'),
+        [Input('threshold-slider', 'value'),
+         Input('well-selector', 'value'),
+         Input('dropdown2', 'value')])
+def callback(threshold, well_idx, rise_or_fall):
+    if rise_or_fall == 'rise':
+        auto_evals = (signals > threshold).argmax(axis=1)
+    elif rise_or_fall == 'fall':
+        auto_evals = signals.shape[1] - (np.fliplr(signals) > threshold).argmax(axis=1)
+    return {
+            'data': [
+                {
+                    'x': list(auto_evals),
+                    'y': list(manual_evals),
+                    'mode': 'markers',
+                    'marker': {'size': 5},
+                    'name': 'Well',
+                },
+                {
+                    'x': [0, len(signals[0, :])],
+                    'y': [0, len(signals[0, :])],
+                    'mode': 'lines',
+                    'name': 'Auto = Manual',
+                },
+                {
+                    'x': [auto_evals[well_idx]],
+                    'y': [manual_evals[well_idx]],
+                    'mode': 'markers',
+                    'marker': {'size': 10},
+                    'name': 'Selected well',
+                },
+            ],
+            'layout': {
+                'title': 'Auto vs Manual',
+                'xaxis': {
+                    'title': 'Auto',
+                },
+                'yaxis': {
+                    'title': 'Manual',
+                },
+                'hovermode': 'closest',
+            },
+        }
+
+
+@app.callback(
+        Output('well-selector', 'value'),
+        [Input('well-slider', 'value')])
+def callback(well_idx):
+    return well_idx
+
+
+@app.callback(
+        Output('well-header', 'children'),
+        [Input('well-selector', 'value')])
+def callback(well_idx):
+    return '{}'.format(well_idx)
+
+
+@app.callback(
+        Output('time-header', 'children'),
+        [Input('signal-graph', 'clickData')])
+def callback(click_data):
+    if click_data is None:
+        return
+    time = click_data['points'][0]['x']
+    return '{}'.format(time)
+
+
+@app.callback(
+        Output('signal-graph', 'figure'),
+        [Input('well-selector', 'value'),
+         Input('threshold-slider', 'value'),
+         Input('dropdown2', 'value')])
+def callback(well_idx, threshold, rise_or_fall):
+    if rise_or_fall == 'rise':
+        auto_evals = (signals > threshold).argmax(axis=1)
+    elif rise_or_fall == 'fall':
+        auto_evals = signals.shape[1] - (np.fliplr(signals) > threshold).argmax(axis=1)
+    return {
+            'data': [
+                {
+                    'x': list(range(len(signals[0, :]))),
+                    'y': list(signals[well_idx]),
+                    'mode': 'markers+lines',
+                    'marker': {'size': 5},
+                    'name': 'Signal',
+                },
+                {
+                    'x': list(range(len(signals[0, :]))),
+                    'y': [threshold]*len(signals[0, :]),
+                    'mode': 'lines',
+                    'name': 'Threshold',
+                },
+                {
+                    'x': [manual_evals[well_idx]]*255,
+                    'y': list(range(256)),
+                    'mode': 'lines',
+                    'name': 'Manual',
+                },
+                {
+                    'x': [auto_evals[well_idx]]*255,
+                    'y': list(range(256)),
+                    'mode': 'lines',
+                    'name': 'Auto',
+                },
+            ],
+            'layout': {
+                'title': '',
+                'xaxis': {
+                    'title': 'Time step',
+                },
+                'yaxis': {
+                    'title': 'Signal',
+                },
+                'showlegend': False,
+            },
+        }
+
+
+@app.callback(
+        Output('org-image', 'src'),
+        [Input('signal-graph', 'clickData'),
+            Input('well-selector', 'value')])
+def callback(click_data, well_idx):
+    if click_data is None:
+        time = 0
+    else:
+        time = click_data['points'][0]['x']
+    orgimg_paths = sorted(glob.glob(
+            os.path.join(data_root, imaging_env, 'original', '*.jpg')))
+    org_img = np.array(
+            PIL.Image.open(orgimg_paths[time]).convert('L'), dtype=np.uint8)
+    r, c = np.where(mask == well_idx)
+    org_img = org_img[r.min():r.max(), c.min():c.max()]
+    org_img = PIL.Image.fromarray(org_img)
+    buf = io.BytesIO()
+    org_img.save(buf, format='JPEG')
+    return 'data:image/jpeg;base64,{}'.format(
+            base64.b64encode(buf.getvalue()).decode('utf-8'))
+
+
+@app.callback(
+        Output('image', 'src'),
+        [Input('signal-graph', 'clickData'),
+         Input('well-selector', 'value')])
+def callback(click_data, well_idx):
+    if click_data is None:
+        time = 0
+    else:
+        time = click_data['points'][0]['x']
+    buf = io.BytesIO()
+    PIL.Image.fromarray(
+            (255 * labels[well_idx, time, :, :]).astype(np.uint8)).save(buf, format='PNG')
+    return 'data:image/png;base64,{}'.format(
+            base64.b64encode(buf.getvalue()).decode('utf-8'))
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
